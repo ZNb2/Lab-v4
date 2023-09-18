446690c2f8c37772d8a67bfbcf6593543d2816c7
ZNb2 <143345498+ZNb2@users.noreply.github.com>
2023-09-18 18:20:30 -0300

    Update central.go

--- Central/central.go
+++ Central/central.go
@@ -202,7 +202,7 @@ func main() {
 
 					log.Printf("Mensaje asíncrono de servidor %s leído\n", subcadenas[0])
 					
-					go ConexionGRPC2(llaves_pedidas,subcadenas[0])
+					ConexionGRPC2(llaves_pedidas,subcadenas[0])
 					num_cola++
 					log.Printf("Se inscribieron %d cupos de servidor %s\n", llaves_pedidas, subcadenas[0])
 					
